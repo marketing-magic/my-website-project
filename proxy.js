655c4d74f0815461331a4fd7ce8f06394863db56
marketing-magic <noyshoshansolomon@gmail.com>
2024-06-14 01:14:57 +0300

    Add files via upload

--- proxy.js
+++ proxy.js
@@ -6,27 +6,57 @@ import fetch from 'node-fetch';
 const app = express();
 const port = 3000;
 
-app.use(cors({ origin: 'https://noyshoshan.i.ng' })); // Enable CORS for your domain
+const client_id = "1000.A74ZA13YR9CIZEFNWB0PB4EK4SFX1B";
+const client_secret = "32f017cf1fed5d7a01892c5f45ef0c04f48be50f89";
+let access_token = "1000.9d2d76c07c12ac0ee9fe3d4b1b17481c.245561d1b2e1bd552a29370806c7fb72";
+const refresh_token = "1000.6be3cc49e03e2e21bfe7083c30075985.56e6d7ea480c225933f75b533880c05c";
+
+app.use(cors({ origin: 'https://noyshoshan.i.ng' }));
 app.use(bodyParser.json());
+
+async function refreshAccessToken() {
+    try {
+        const response = await fetch('https://accounts.zoho.com/oauth/v2/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams({
+                refresh_token: refresh_token,
+                client_id: client_id,
+                client_secret: client_secret,
+                grant_type: 'refresh_token'
+            })
+        });
+
+        const data = await response.json();
+        access_token = data.access_token;
+        console.log('Access token refreshed:', access_token);
+    } catch (error) {
+        console.error('Error refreshing access token:', error.message);
+    }
+}
 
 app.post('/proxy', async (req, res) => {
     try {
-        console.log('Received request:', req.body); // Print the request body
+        await refreshAccessToken();
+
+        console.log('Received request:', req.body);
 
         const response = await fetch('https://www.zohoapis.com/crm/v2/Leads', {
             method: 'POST',
             headers: {
-                'Authorization': 'Bearer 1000.fe76cf37e7cc295362d15947bd43eaa5.a19e49d2da98392a75cead44be30f756', // השתמשי ב-Access Token שקיבלת
+                'Authorization': 'Bearer ' + access_token,
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(req.body)
         });
 
         const data = await response.json();
-        console.log('Response from Zoho:', data); // Print the response from Zoho
+        console.log('Response from Zoho:', data);
         res.json(data);
     } catch (error) {
-        console.error('Error:', error.message); // Print the error message
+        console.error('Error:', error.message);
         res.status(500).json({ error: error.message });
     }
 });
